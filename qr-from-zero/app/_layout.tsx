5e362b06864f9be1e86701a8bbd0ee047c15c30e
YAN <2914566893@qq.com>
2026-01-29 15:49:58 +0800

    组件库集成

--- qr-from-zero/app/_layout.tsx
+++ qr-from-zero/app/_layout.tsx
@@ -3,13 +3,18 @@ import { AuthProvider } from '@/contexts/AuthContext';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import {GestureHandlerRootView} from "react-native-gesture-handler";
 
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+
 export default function RootLayout() {
   return (
       <GestureHandlerRootView>
           <SafeAreaProvider>
               <AuthProvider>
-                  <Slot />
-              </AuthProvider>
+    <GluestackUIProvider mode="dark">
+      <Slot />
+    </GluestackUIProvider>
+    </AuthProvider>
           </SafeAreaProvider>
       </GestureHandlerRootView>
   );
